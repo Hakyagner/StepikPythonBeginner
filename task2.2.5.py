3a19f427ac54c655d772d8c47a1e85d419319bf8
Hakyagner <katy.roj.learn@mail.ru>
2024-05-26 14:15:21 +0300

    fix remaks

--- task2.2.5.py
+++ task2.2.5.py
@@ -1,4 +1,4 @@
-name = input("Веди своё имя")
-print("Привет,",name)
+name = input("Введи своё имя: ")
+print("Привет,", name)
 
-# Соблюдай правила форматирования (https://stepik.org/lesson/275252/step/18?unit=256355) и русского языка
+
