6a8bd45abf1c47f6f3ddc3240243b7f44b13ea86
Hakyagner <katy.roj.learn@mail.ru>
2024-05-26 12:57:27 +0300

    выполнено 2.3.3

---
+++ task2.3.3.py
@@ -0,0 +1,6 @@
+rasdelitel = input("Напиши любой разделитель")
+str1 = input("Напиши любое слово")
+str2 = input("Напиши любое слово")
+str3 = input("Напиши любое слово")
+
+print(str1, str2, str3, sep=rasdelitel)
