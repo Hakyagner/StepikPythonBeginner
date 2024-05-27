484b388b01b8bee0b2da66466b4a6f3f7ab6d8bf
Hakyagner <katy.roj.learn@mail.ru>
2024-05-27 22:11:53 +0300

    fix remaks

--- task2.3.3.py
+++ task2.3.3.py
@@ -1,6 +1,6 @@
-separator = input("Напиши любой разделитель: ")
-str1 = input("Напиши любое слово: ")
-str2 = input("Напиши любое слово: ")
-str3 = input("Напиши любое слово: ")
+separator = input("Напиши разделитель: ")
+str1 = input("Напиши слово: ")
+str2 = input("Напиши слово: ")
+str3 = input("Напиши слово: ")
 
 print(str1, str2, str3, sep=separator)
