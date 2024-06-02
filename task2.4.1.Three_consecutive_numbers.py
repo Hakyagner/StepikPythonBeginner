b1cfe79615b7d74a482e43690409b3bb6f39aef0
Hakyagner <katy.roj.learn@mail.ru>
2024-06-02 20:53:29 +0300

    выполнено 2.4.1

--- task2.4.1.Three_consecutive_numbers.py
+++ task2.4.1.Three_consecutive_numbers.py
@@ -1,4 +1,13 @@
-num = int(input("Напиши число: "))
-print(num)
-print("Cледующее за числом", num, "-", num + 1)
-print("Cледующее за числом", num + 1, "-", num + 1 + 1)
+print("Три последовательных числа")
+print()
+while True:
+    num = input("Напиши число: ")
+    if num.isdigit():
+        num = int(num)
+        print(num)
+        print("Cледующее за числом", num, "-", num + 1)
+        print("Cледующее за числом", num + 1, "-", num + 1 + 1)
+        break
+    else:
+        print("Нужно ввести целое положительное число")
+        print()
