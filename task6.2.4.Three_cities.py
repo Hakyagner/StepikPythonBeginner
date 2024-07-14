a58bf359144ede589db79d290acecb8df07d5f77
Hakyagner <katy.roj.learn@mail.ru>
2024-07-15 01:12:59 +0300

    выполнено 6.2.4

---
+++ task6.2.4.Three_cities.py
@@ -0,0 +1,27 @@
+print("Три города")
+print()
+
+while True:
+    city1 = input("Введите названи первово города: ")
+    if len(city1) > 0:
+        break
+    else:
+        print("Ошибка! Введите название города, у которога длина символов не равна 0. ")
+while True:
+    city2 = input("Введите названи второго города: ")
+    if len(city2) > 0:
+        break
+    else:
+        print("Ошибка! Введите название города, у которога длина символов не равна 0. ")
+while True:
+    city3 = input("Введите названи третьего города: ")
+    if len(city3) > 0:
+        break
+    else:
+        print("Ошибка! Введите название города, у которога длина символов не равна 0. ")
+print()
+
+maxsimum = max(len(city1), len(city2), len(city3))
+minimum = min(len(city1), len(city2), len(city3))
+
+print(f"Из названий трёх годов -  {city1, city2, city3} самое длиное название у города - {maxsimum} самое короткое название у города - {minimum}.")
