a0f0d3ce48715f25ac012a5bd5dcda94ae18eced
Hakyagner <katy.roj.learn@mail.ru>
2024-07-15 00:51:20 +0300

    выполнено 6.2.2

---
+++ task6.2.2.What_Your_Name.py
@@ -0,0 +1,7 @@
+print("What's Your Name?")
+print()
+
+name = input("Введите своё имя: ")
+surname = input("Введите свою фамилию: ")
+
+print(f"Hello {name, surname}! You have just delved into Python.")
