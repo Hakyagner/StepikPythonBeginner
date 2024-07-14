b9d9a1e2e92de1bcc9a67e2d5cdde3e1e4439d4d
Hakyagner <katy.roj.learn@mail.ru>
2024-07-15 00:43:42 +0300

    выполнено 6.1.12

---
+++ task6.2.1.Concatenation.py
@@ -0,0 +1,11 @@
+print("Конкатенация")
+print()
+
+str1 = '"Python is a great language!", said Fred. '
+str2 = '"I don'
+str3 = "'"
+str4 = 't ever remember having this much fun before."'
+
+total = str1 + str2 + str3 + str4
+
+print(total)
