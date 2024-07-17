88b34ead67b07d389483ede972f4ef594bf187ec
Hakyagner <katy.roj.learn@mail.ru>
2024-07-18 00:19:51 +0300

    выполнено 7.1.6

---
+++ task7.1.6.square_of_the_number.py
@@ -0,0 +1,19 @@
+import ifnumber
+print("Квадрат числа")
+print()
+
+while True:
+    n = input('Введи число: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int':
+        n = int(n)
+        break
+    elif if_number == 'float':
+        n = float(n)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести число')
+    print()
+
+for i in range(n + 1):
+    print(f"Квадрат числа {i} равен {i ** 2}.")
