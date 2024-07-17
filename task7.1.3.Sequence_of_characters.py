ee06bdbbe06698a5d6b53b4be5bb83e487d6af59
Hakyagner <katy.roj.learn@mail.ru>
2024-07-18 00:06:17 +0300

    выполнено 7.1.3

---
+++ task7.1.3.Sequence_of_characters.py
@@ -0,0 +1,18 @@
+print("Последовательность символов")
+print()
+
+n1 = "AAA"
+n2 = "BBBB"
+n3 = "TTTTT"
+
+for i in range(6):
+    print(n1)
+for i in range(5):
+    print(n2)
+
+print("E")
+
+for i in range(9):
+    print(n3)
+
+print("G")
