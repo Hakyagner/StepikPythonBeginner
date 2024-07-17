6d5b30c2d8369b239243aee232438e9ef28c7550
Hakyagner <katy.roj.learn@mail.ru>
2024-07-18 00:00:15 +0300

    выполнено 7.1.2

---
+++ task7.1.2.Repeat_after_me_1.py
@@ -0,0 +1,22 @@
+import ifnumber
+print("Повторяй за мной 1")
+print()
+
+string = input("Введите строку: ")
+
+while True:
+    num = input('Введите сколько раз надо будет повтарить вашу введёную строку: ')
+    if_number = ifnumber.if_number(num)
+    if if_number == 'int':
+        num = int(num)
+        break
+    elif if_number == 'float':
+        num = float(num)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести число')
+    print()
+print(num)
+
+for i in range(num):
+    print(num)
