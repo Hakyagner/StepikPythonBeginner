7f3542218ed9cb80dbf85a9b0a4c8c63c7446d4c
Hakyagner <katy.roj.learn@mail.ru>
2024-07-18 22:40:48 +0300

    выполнено 7.2.3

---
+++ task7.2.3.Sequence_of_numbers_3.py
@@ -0,0 +1,26 @@
+import ifnumber
+print("Последовательность чисел 3")
+print()
+
+while True:
+    m = input('Введи первое число: ')
+    if_number = ifnumber.if_number(m)
+    if if_number == 'int':
+        m = int(m)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое число')
+while True:
+    n = input('Введи первое число: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int':
+        n = int(n)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое число')
+    print()
+print()
+
+for i in range(m, n, -2):
+    if i % 2 != 0:
+        print(f"Все нечётные целые числа от {m} до {n} (включительно) - {i}")
