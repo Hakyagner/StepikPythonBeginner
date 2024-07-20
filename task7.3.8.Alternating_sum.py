c79afe6ccac641a9b705c27935b46e69aa17b4e8
Hakyagner <katy.roj.learn@mail.ru>
2024-07-20 21:47:57 +0300

    выполнено 7.3.8

---
+++ task7.3.8.Alternating_sum.py
@@ -0,0 +1,27 @@
+import ifnumber
+print("Знакочередующаяся сумма")
+print()
+
+while True:
+    n = input('Введи число: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int':
+        n = int(n)
+        print()
+        break
+    elif if_number == 'float':
+        n = float(n)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести число')
+    print()
+
+total = 0
+
+for i in range(1, n + 1):
+    if n % 2 == 0:
+        total -= i
+    else:
+        total += i
+
+print(total)
