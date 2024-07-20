c496d66a29fe8ffd785bef534c306b298064d3ed
Hakyagner <katy.roj.learn@mail.ru>
2024-07-20 21:36:30 +0300

    выполнено 7.3.7

---
+++ task7.3.7.Sum_of_divisors.py
@@ -0,0 +1,25 @@
+import ifnumber
+print("Сумма делителей")
+print()
+
+while True:
+    n = input('Введи число: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int':
+        n = int(n)
+        print()
+        break
+    elif if_number == 'float':
+        n = float(n)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести число')
+    print()
+
+total = 0
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        total += i
+
+print(f"Сумма всех делителей числа {n} ровна {total}.")
