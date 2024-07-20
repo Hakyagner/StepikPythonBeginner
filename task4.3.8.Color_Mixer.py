daa39078675ce8b62711afe4acf9ab618693d019
Hakyagner <katy.roj.learn@mail.ru>
2024-07-20 22:29:37 +0300

    исправила 4.3.8

--- task4.3.8.Color_Mixer.py
+++ task4.3.8.Color_Mixer.py
@@ -4,20 +4,23 @@ print()
 color1 = input("Введите название первого цвета: ")
 color2 = input("Введите название второго цвета: ")
 
-if color1.lower() != "красный" and color1.lower() != "синий" and color1.lower() != "жёлтый":
+if color1[1] == "e":
+    color1[1] = "ё"
+if color2[1] == "е":
+    color2[1] = "ё"
+if (color1.lower() != "красный" and color1.lower() != "синий" and color1.lower() != "жёлтый") or (color2.lower() != "красный" and color2.lower() != "синий" and color2.lower() != "жёлтый"):
     print("Ошибка. Этот цвет не основной. Надо было ввести (красный или синий, или жёлтый)!")
-elif color2.lower() != "красный" and color2.lower() != "синий" and color2.lower() != "жёлтый":
-    print("Ошибка. Этот цвет не основной. Надо было ввести (красный или синий, или жёлтый)!")
-elif color1.lower() == "красный" and color2.lower() == "синий":
-    print(f"Из цветов {color1} и {color2} получается - фиолетовый")
-elif color1.lower() == "синий" and color2.lower() == "красный":
-    print(f"Из цветов {color1} и {color2} получается - фиолетовый")
-elif (color1.lower() == "красный" and color2.lower() == "жёлтый") or (color2.lower() == "красный" and color1.lower() == "жёлтый"):
-    print(f"Из цветов {color1} и {color2} получается - оранжевый")
-elif (color1.lower() == "жёлтый" and color2.lower() == "синий") or (color2.lower() == "жёлтый" and color1.lower() == "синий"):
-    print(f"Из цветов {color1} и {color2} получается - зелёный")
-elif color1 == color2:
-    print(f"Из цветов {color1} и {color2} получается - {color1}")
+else:
+    if color1.lower() == "красный" and color2.lower() == "синий":
+        print(f"Из цветов {color1} и {color2} получается - фиолетовый")
+    elif color1.lower() == "синий" and color2.lower() == "красный":
+        print(f"Из цветов {color1} и {color2} получается - фиолетовый")
+    elif (color1.lower() == "красный" and color2.lower() == "жёлтый") or (color2.lower() == "красный" and color1.lower() == "жёлтый"):
+        print(f"Из цветов {color1} и {color2} получается - оранжевый")
+    elif (color1.lower() == "жёлтый" and color2.lower() == "синий") or (color2.lower() == "жёлтый" and color1.lower() == "синий"):
+        print(f"Из цветов {color1} и {color2} получается - зелёный")
+    elif color1 == color2:
+        print(f"Из цветов {color1} и {color2} получается - {color1}")
 
 # Оптимизируй, чтобы каждый print был уникальным
-# Внимательно с and/or
+# Внимательно с and/or if "ё" in color1 or "ё" in color2:
