a37037ade3bd28c26cfc52e0e5c6516fb0753f75
Alex Ko <help.mail015@gmail.com>
2024-07-31 11:31:21 +0300

    Find errors

--- task6.1.3.The_reverse_number.py
+++ task6.1.3.The_reverse_number.py
@@ -3,23 +3,21 @@ print("Обратное число")
 print()
 
 while True:
-    num = input("Введите положительное число: ")
+    num = input("Введите число: ")
     if_number = ifnumber.if_number(num)
     if if_number == 'int':
         num = int(num)
         print()
         break
-    if if_number == 'float':
+    elif if_number == 'float':
         num = float(num)
         print()
         break
     else:
-        print("Нужно ввести положительное число")
+        print("Нужно ввести число")
         print()
 
 if num == 0:
     print('Обратного числа не существует')
 else:
     print(f"Обратное числу {num} = {1 / num}.")
-
-# done
