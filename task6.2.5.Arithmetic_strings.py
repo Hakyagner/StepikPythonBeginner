e57f296f01061485acfddd6775108692d9b08ed6
Hakyagner <katy.roj.learn@mail.ru>
2024-08-15 14:23:30 +0300

    fix 6.2.5

--- task6.2.5.Arithmetic_strings.py
+++ task6.2.5.Arithmetic_strings.py
@@ -1,18 +1,12 @@
 print("Арифметические строки")
 print()
 
-str1 = len(input("Введите первую строку: "))
-str2 = len(input("Введите вторую строку: "))
-str3 = len(input("Введите третью строку: "))
+str_len_1 = len(input("Введите первую строку: "))
+str_len_2 = len(input("Введите вторую строку: "))
+str_len_3 = len(input("Введите третью строку: "))
 
-max_len = max(str1, str2, str3)
-min_len = min(str1, str2, str3)
-average_len = str1 + str2 + str3 - max_len - min_len
-
-if average_len - min_len == max_len - average_len:
-    print(f"Из длин этих строк - {str1}, {str2}, {str3} можно построить арифметическую прогрессию.")
+if abs(str_len_2 - str_len_1) == abs(str_len_3 - str_len_2) or abs(str_len_2 - str_len_3) == abs(str_len_3 - str_len_1) or abs(str_len_2 - str_len_1) == abs(str_len_3 - str_len_1):
+    print(f"Из длин этих строк - {str_len_1}, {str_len_2}, {str_len_3} можно построить арифметическую прогрессию.")
 else:
-    print(f"Из длин этих строк - {str1}, {str2}, {str3} нельзя построить арифметическую прогрессию.")
-
-# НЕПРАВИЛЬНО!!!
-# проверять свои решения не пробовала????
+    print(f"Из длин этих строк - {str_len_1}, {str_len_2}, {str_len_3} нельзя построить арифметическую прогрессию.")
+# done
