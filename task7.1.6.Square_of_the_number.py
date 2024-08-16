ccf0627feb266b957692946b41bb03b279701707
Hakyagner <katy.roj.learn@mail.ru>
2024-08-16 12:46:41 +0300

    fix 7.1.6

--- task7.1.6.Square_of_the_number.py
+++ task7.1.6.Square_of_the_number.py
@@ -4,19 +4,16 @@ print("Квадрат числа")
 print()
 
 while True:
-    n = input('Введи число: ')
+    n = input('Введи целое положительное число: ')
     if_number = ifnumber.if_number(n)
-    if if_number == 'int':
+    if if_number == 'int' and int(n) > 0:
         n = int(n)
         break
-    elif if_number == 'float':
-        n = float(n)
-        break
     else:
-        print('Данные введены некорректно! Нужно ввести число')
+        print('Данные введены некорректно! Нужно ввести целое положительное число')
     print()
 
 for i in range(n + 1):
-    print(f"Квадрат числа {i} равен {i ** 2}.")
+    print(f"Квадрат числа {i} равен {i ** 2}")
 
-# Какое число нужно ввести?
+# done
