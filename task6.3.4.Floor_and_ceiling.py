d355472cfdc8383f9b193c1fdd0c0c5aa54ef027
Hakyagner <katy.roj.learn@mail.ru>
2024-08-16 12:45:54 +0300

    fix 6.3.4

--- task6.3.4.Floor_and_ceiling.py
+++ task6.3.4.Floor_and_ceiling.py
@@ -5,21 +5,20 @@ print("Пол и потолок")
 print()
 
 while True:
-    x = input('Введите положительное число: ')
+    x = input('Введите число: ')
     if_number = ifnumber.if_number(x)
-    if if_number == 'int' and int(x) > 0:
+    if if_number == 'int':
         x = int(x)
         break
-    elif if_number == 'float' and float(x) > 0:
+    elif if_number == 'float':
         x = float(x)
         break
     else:
-        print('Данные введены некорректно! Нужно ввести положительное число')
+        print('Данные введены некорректно! Нужно ввести число')
     print()
 
 total = ceil(x) + floor(x)
 
-print(f"{x} + {x} = {total}")
+print(f"⌊{x}⌋ + ⌈{x}⌉ = {total}")
 
-# число может быть любым
-# print неправильный
+# done
