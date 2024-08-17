245b7a723186ee91ed55d16eb8a99ef20bc7b86d
Hakyagner <katy.roj.learn@mail.ru>
2024-08-17 12:28:31 +0300

    fix 6.2.5, 6.3.5

--- task6.2.5.Arithmetic_strings.py
+++ task6.2.5.Arithmetic_strings.py
@@ -1,13 +1,16 @@
 print("Арифметические строки")
 print()
-
-str_len_1 = len(input("Введите первую строку: "))
-str_len_2 = len(input("Введите вторую строку: "))
-str_len_3 = len(input("Введите третью строку: "))
+str_1 = input("Введите первую строку: ")
+str_2 = input("Введите вторую строку: ")
+str_3 = input("Введите третью строку: ")
+str_len_1 = len(str_1)
+str_len_2 = len(str_2)
+str_len_3 = len(str_3)
 
 if abs(str_len_2 - str_len_1) == abs(str_len_3 - str_len_2) or abs(str_len_2 - str_len_3) == abs(str_len_3 - str_len_1) or abs(str_len_2 - str_len_1) == abs(str_len_3 - str_len_1):
-    print(f"Из длин этих строк - {str_len_1}, {str_len_2}, {str_len_3} можно построить арифметическую прогрессию.")
+    ins = "можно"
 else:
-    print(f"Из длин этих строк - {str_len_1}, {str_len_2}, {str_len_3} нельзя построить арифметическую прогрессию.")
-
-# из длин каких строк?
+    ins = "нельзя"
+msg = f"Из длин этих строк - <{str_1}>, <{str_2}>, <{str_3}> {ins} построить арифметическую прогрессию."
+print(msg)
+# done
