21d306f562dc185ebc0d4d1f2bf237b29ac97990
Hakyagner <katy.roj.learn@mail.ru>
2024-08-18 13:34:40 +0300

    fix 7.2.1

--- task7.2.1.Sequence_of_numbers_1.py
+++ task7.2.1.Sequence_of_numbers_1.py
@@ -3,29 +3,33 @@ print("Последовательность чисел 1")
 print()
 
 while True:
-    m = input('Введи первое число: ')
-    if_number = ifnumber.if_number(m)
-    if if_number == 'int':
-        m = int(m)
+    print("Нужно ввести два целых числа. Второе число должно быть не меньше первого.")
+    while True:
+        m = input('Введи первое число: ')
+        if_number = ifnumber.if_number(m)
+        if if_number == 'int':
+            m = int(m)
+            break
+        else:
+            print('Данные введены некорректно! Нужно ввести целое число')
+    while True:
+        n = input('Введи второе число: ')
+        if_number = ifnumber.if_number(n)
+        if if_number == 'int':
+            n = int(n)
+            break
+        else:
+            print('Данные введены некорректно! Нужно ввести целое число')
+        print()
+    if m <= n:
         break
     else:
-        print('Данные введены некорректно! Нужно ввести целое число')
-while True:
-    n = input('Введи первое число: ')
-    if_number = ifnumber.if_number(n)
-    if if_number == 'int':
-        n = int(n)
-        break
-    else:
-        print('Данные введены некорректно! Нужно ввести целое число')
+        print('Данные введены некорректно! Второе число должно быть не меньше первого.')
     print()
 print()
 
-if m < n:
-    for i in range(m, n + 1):
-        print(f"Целые числа от {m} до {n} (включительно) - {i}.")
-else:
-    for i in range(n, m + 1):
-        print(f"Целые числа от {n} до {m} (включительно) - {i}.")
+print(f"Целые числа от {m} до {n}")
+for i in range(m, n + 1):
+    print(i)
 
 # Неправильно. Но по условию задачи m ≤ n
