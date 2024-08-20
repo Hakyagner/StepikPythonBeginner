115e4c68582a8d571f8c84aea9ebda22ce402df2
Hakyagner <katy.roj.learn@mail.ru>
2024-08-20 13:05:54 +0300

    fix 7.2.4

--- task7.2.4.Sequence_of_numbers_4.py
+++ task7.2.4.Sequence_of_numbers_4.py
@@ -5,24 +5,25 @@ print()
 while True:
     m = input('Введи первое число: ')
     if_number = ifnumber.if_number(m)
-    if if_number == 'int':
+    if if_number == 'int' and int(m) > 0:
         m = int(m)
         break
     else:
-        print('Данные введены некорректно! Нужно ввести целое число')
+        print('Данные введены некорректно! Нужно ввести целое положительное число')
 while True:
-    n = input('Введи первое число: ')
+    n = input('Введи второе число: ')
     if_number = ifnumber.if_number(n)
-    if if_number == 'int':
+    if if_number == 'int' and int(n) > 0:
         n = int(n)
         break
     else:
-        print('Данные введены некорректно! Нужно ввести целое число')
+        print('Данные введены некорректно! Нужно ввести целое положительное число')
     print()
 print()
 
+print(f"Целые числа от {m} до {n}:")
 for i in range(m, n + 1):
     if i % 17 == 0 or i % 10 == 9 or i % 15 == 0:
-        print(f"Целые числа от {m} до {n} (включительно) - {i}.")
+        print(i)
 
-# Неправильно
+# done
