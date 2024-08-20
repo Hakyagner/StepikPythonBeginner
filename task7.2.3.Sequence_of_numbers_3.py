fd0120d9bb2e0ac1dbf8a0ad4305f882746bd244
Hakyagner <katy.roj.learn@mail.ru>
2024-08-20 12:58:57 +0300

    fix 7.2.3

--- task7.2.3.Sequence_of_numbers_3.py
+++ task7.2.3.Sequence_of_numbers_3.py
@@ -1,28 +1,35 @@
 import ifnumber
 print("Последовательность чисел 3")
 print()
-
 while True:
-    m = input('Введи первое число: ')
-    if_number = ifnumber.if_number(m)
-    if if_number == 'int':
-        m = int(m)
+    print(f"Нужно ввести два числа, первое число больше второго.")
+    while True:
+        m = input('Введи первое число: ')
+        if_number = ifnumber.if_number(m)
+        if if_number == 'int':
+            m = int(m)
+            break
+        else:
+            print('Данные введены некорректно! Нужно ввести целое число')
+    while True:
+        n = input('Введи первое число: ')
+        if_number = ifnumber.if_number(n)
+        if if_number == 'int':
+            n = int(n)
+            break
+        else:
+            print('Данные введены некорректно! Нужно ввести целое число')
+        print()
+    if m > n:
         break
     else:
-        print('Данные введены некорректно! Нужно ввести целое число')
-while True:
-    n = input('Введи первое число: ')
-    if_number = ifnumber.if_number(n)
-    if if_number == 'int':
-        n = int(n)
-        break
-    else:
-        print('Данные введены некорректно! Нужно ввести целое число')
-    print()
+        print('Данные введены некорректно! Первое число больше второго.')
+        print()
 print()
 
-for i in range(m, n, -2):
+print(f"Все нечётные целые числа от {m} до {n}:")
+for i in range(m, n, -1):
     if i % 2 != 0:
-        print(f"Все нечётные целые числа от {m} до {n} (включительно) - {i}")
+        print(i)
 
-# Неправильно
+# done
