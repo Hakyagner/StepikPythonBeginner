90fd5aa951bba95daca75143a8977aa56c81936f
Hakyagner <katy.roj.learn@mail.ru>
2024-08-21 21:37:11 +0300

    fix 7.3.1

--- task7.3.1.Amount_of_numbers.py
+++ task7.3.1.Amount_of_numbers.py
@@ -22,10 +22,10 @@ while True:
 print()
 
 total = 0
-
-for i in range(a, b + 1):
-    if i ** 3 % 10 == 4 or i ** 3 % 10 == 9:
-        total += 1
+if a <= b:
+    for i in range(a, b + 1):
+        if i ** 3 % 10 == 4 or i ** 3 % 10 == 9:
+            total += 1
 
 print(f"Количество чисел в диапазоне от {a} до {b} (включительно), куб которых оканчивается на 4 или 9, равно {total}.")
 
