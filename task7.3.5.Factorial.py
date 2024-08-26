ddd3f7355f5e2c28bda2466c622890116f303cca
Hakyagner <katy.roj.learn@mail.ru>
2024-08-26 22:02:48 +0300

    fix 7.3.5

--- task7.3.5.Factorial.py
+++ task7.3.5.Factorial.py
@@ -3,22 +3,21 @@ print("Факториал")
 print()
 
 while True:
-    n = input('Введи число: ')
+    n = input('Введи целое положительное число: ')
     if_number = ifnumber.if_number(n)
-    if if_number == 'int':
+    if if_number == 'int' and int(n) > 0:
         n = int(n)
         break
     else:
-        print('Данные введены некорректно! Нужно ввести целое число')
+        print('Данные введены некорректно! Нужно ввести целое положительное число')
     print()
 print()
 
-total = 0
+total = 1
 
 for i in range(1, n + 1):
     total *= i
 
-print(f"{n}! = {total}.")
+print(f"{n} равно {total}.")
 
-# Требования к числу?
-# Неверное решение
+# done
