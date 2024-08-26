b81af589696dfd3f2b08b6e9adc5a69d21c7ba22
Hakyagner <katy.roj.learn@mail.ru>
2024-08-26 21:57:47 +0300

    fix 7.2.4

--- task7.2.4.Sequence_of_numbers_4.py
+++ task7.2.4.Sequence_of_numbers_4.py
@@ -3,29 +3,34 @@ print("Последовательность чисел 4")
 print()
 
 while True:
-    m = input('Введи первое число: ')
-    if_number = ifnumber.if_number(m)
-    if if_number == 'int' and int(m) > 0:
-        m = int(m)
+    print("Нужно ввести два числа, первое число не больше второго.")
+    while True:
+        m = input('Введи первое число: ')
+        if_number = ifnumber.if_number(m)
+        if if_number == 'int' and int(m) > 0:
+            m = int(m)
+            break
+        else:
+            print('Данные введены некорректно! Нужно ввести целое положительное число')
+    while True:
+        n = input('Введи второе число: ')
+        if_number = ifnumber.if_number(n)
+        if if_number == 'int' and int(n) > 0:
+            n = int(n)
+            break
+        else:
+            print('Данные введены некорректно! Нужно ввести целое положительное число')
+        print()
+    if m <= n:
         break
     else:
-        print('Данные введены некорректно! Нужно ввести целое положительное число')
-while True:
-    n = input('Введи второе число: ')
-    if_number = ifnumber.if_number(n)
-    if if_number == 'int' and int(n) > 0:
-        n = int(n)
-        break
-    else:
-        print('Данные введены некорректно! Нужно ввести целое положительное число')
+        print('Данные введены некорректно! Первое число не больше второго.')
     print()
 print()
-if m <= n:
-    print(f"Целые числа от {m} до {n}:")
-    for i in range(m, n + 1):
-        if i % 17 == 0 or i % 10 == 9 or i % 15 == 0:
-            print(i)
-else:
-    print("Ошибка")
 
-# неправильно. Числа какими должны быть по отношению друг к другу? Где в задании сказано, что нужно выводить ошибку?
+print(f"Целые числа от {m} до {n}:")
+for i in range(m, n + 1):
+    if i % 17 == 0 or i % 10 == 9 or i % 15 == 0:
+        print(i)
+
+# done
