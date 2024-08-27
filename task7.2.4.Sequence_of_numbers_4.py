ddad8f725c3286cd7c9e83becfba46d4a5121481
Hakyagner <katy.roj.learn@mail.ru>
2024-08-27 14:23:23 +0300

    fix 7.2.4

--- task7.2.4.Sequence_of_numbers_4.py
+++ task7.2.4.Sequence_of_numbers_4.py
@@ -30,7 +30,7 @@ print()
 
 print(f"Целые числа от {m} до {n}:")
 for i in range(m, n + 1):
-    if i % 17 == 0 or i % 10 == 9 or i % 15 == 0:
+    if i % 17 == 0 or i % 10 == 9 or (i % 3 == 0 and i % 5 == 0):
         print(i)
 
 # done
