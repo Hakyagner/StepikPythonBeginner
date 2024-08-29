00a985e6b2806d07217d13e6a51085bc35e8210d
Hakyagner <katy.roj.learn@mail.ru>
2024-08-30 01:12:49 +0300

    fix 7.3.6

--- task7.3.6.No_zeros.py
+++ task7.3.6.No_zeros.py
@@ -5,18 +5,23 @@ print()
 total = 1
 for i in range(10):
     while True:
-        num = input('Введи целое число: ')
+        if i == 0:
+            num = input('Введи первое число: ')
+        else:
+            num = input('Введи следующее число: ')
         if_number = ifnumber.if_number(num)
-        if if_number == 'int' or (num[0] == "-" and num[1:].isdigit()):
+        if if_number == 'int':
             num = int(num)
             break
+        elif if_number == 'float':
+            num = float(num)
+            break
         else:
-            print('Данные введены некорректно! Нужно ввести целое число')
+            print('Данные введены некорректно! Нужно ввести число')
         print()
     if num != 0:
         total *= num
 
 print(f"Произведение отличных от нуля чисел равно {total}")
 
-# Число каким должно быть по условию задачи? И проверка числа неправильная
-# Так как у тебя всё в цикле, сделай так, чтобы для первого числа у пользователя просили "Введи первое число", а для остальных = "Введи следующее число"
+# done
