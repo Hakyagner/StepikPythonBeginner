63854a595fd177e3854020857566de6d33ffb273
Hakyagner <katy.roj.learn@mail.ru>
2024-08-30 01:13:00 +0300

    fix 7.3.7

---
+++ task7.3.7.Sum_of_divisors.py
@@ -0,0 +1,21 @@
+import ifnumber
+print("Сумма делителей")
+print()
+
+while True:
+    n = input('Введи целое положительное число: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int' and int(n) > 0:
+        n = int(n)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое положительное число')
+    print()
+print()
+
+total = 0
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        total += i
+print(f"Сумма всех делителей {n} равна {total}")
