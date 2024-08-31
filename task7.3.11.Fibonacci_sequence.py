45543e3c31e343647f0b4bf2ce36671d8c202109
Hakyagner <katy.roj.learn@mail.ru>
2024-08-31 17:39:36 +0300

    fix 7.3.11

---
+++ task7.3.11.Fibonacci_sequence.py
@@ -0,0 +1,28 @@
+import ifnumber
+print("Последовательность Фибоначчи")
+print()
+
+while True:
+    n = input('Введи целое положительное число: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int' and int(n) > 0:
+        n = int(n)
+        print()
+        break
+    elif if_number == 'float' and float(n) > 0:
+        n = float(n)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое положительное число')
+    print()
+
+a = 0
+b = 1
+for i in range(n):
+    if n == 1:
+        print('1')
+    elif n == 2:
+        print(1, 1)
+    else:
+        a, b = b, a + b
+        print({a}, end=' ')
