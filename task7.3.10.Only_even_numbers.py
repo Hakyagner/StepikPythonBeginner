4032a526a38c8c1c89af7fe9a320eb2b37396fc2
Hakyagner <katy.roj.learn@mail.ru>
2024-09-01 20:51:18 +0300

    fix 7.3.10

--- task7.3.10.Only_even_numbers.py
+++ task7.3.10.Only_even_numbers.py
@@ -2,4 +2,25 @@ import ifnumber
 print("Only even numbers")
 print()
 
-# Решаешь со мной рядом
+sequence = ""
+counter = 0
+for i in range(10):
+    while True:
+        n = input(f'Введи целое число {i + 1}: ')
+        if_number = ifnumber.if_number(n)
+        if if_number == 'int':
+            sequence += (n + " ")
+            n = int(n)
+            break
+        else:
+            print('Данные введены некорректно! Нужно ввести целое число')
+        print()
+    if n % 2 == 0:
+        counter += 1
+if counter == 10:
+    temp = ""
+else:
+    temp = "не"
+print(f"В последовательности {sequence}{temp} все числа чётные")
+
+# done
