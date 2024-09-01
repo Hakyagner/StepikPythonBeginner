c18ecfcd02e5598d0f5e16de59db731c100f6e8b
Hakyagner <katy.roj.learn@mail.ru>
2024-09-01 20:56:48 +0300

    fix 7.4.1

---
+++ task7.4.1.Till_END.py
@@ -0,0 +1,9 @@
+print("До КОНЦА")
+print()
+
+sequence = ""
+text = input("Введи строку:")
+while text.upper() != 'КОНЕЦ':
+    sequence += (text + " ")
+    print(f"Члены {sequence}последовательности равны {text}")
+    text = input("Введи строку:")
