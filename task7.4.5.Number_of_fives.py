432e4b7b13fe95432a76d71917107e4bc99f11d3
Hakyagner <katy.roj.learn@mail.ru>
2024-09-02 19:40:29 +0300

    fix 7.4.5

---
+++ task7.4.5.Number_of_fives.py
@@ -0,0 +1,31 @@
+import ifnumber
+print("Количество пятерок")
+print()
+
+while True:
+    n = input('Введи целое число: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int':
+        n = int(n)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое число')
+    print()
+print()
+
+total = 0
+
+while 0 < n < 6:
+    if n == 5:
+        total += 1
+    while True:
+        n = input('Введи целое число: ')
+        if_number = ifnumber.if_number(n)
+        if if_number == 'int':
+            n = int(n)
+            break
+        else:
+            print('Данные введены некорректно! Нужно ввести целое число')
+        print()
+
+print(f"Количество пятерок в данной последовательности {total}")
