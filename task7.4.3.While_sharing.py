81f4693a200f8001a0e6c368c943ab1cd3a3f3eb
Hakyagner <katy.roj.learn@mail.ru>
2024-09-02 16:43:41 +0300

    fix 7.4.3

---
+++ task7.4.3.While_sharing.py
@@ -0,0 +1,27 @@
+import ifnumber
+print("Пока делимся")
+print()
+
+while True:
+    n = input('Введи целое число: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int':
+        n = int(n)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое число')
+    print()
+print()
+
+while n % 7 == 0 and int(n) != 0:
+    print(f"{n}")
+    while True:
+        n = input('Введи целое число: ')
+        if_number = ifnumber.if_number(n)
+        if if_number == 'int':
+            n = int(n)
+            break
+        else:
+            print('Данные введены некорректно! Нужно ввести целое число')
+        print()
+    print()
