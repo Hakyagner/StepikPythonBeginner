70c7cbefaa38e043fe62257c9617d9c46b816ef4
Hakyagner <katy.roj.learn@mail.ru>
2024-09-02 16:43:49 +0300

    fix 7.4.4

---
+++ task7.4.4.Sum_of_numbers.py
@@ -0,0 +1,30 @@
+import ifnumber
+print("Сумма чисел")
+print()
+
+while True:
+    n = input('Введи целое число: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int':
+        n = int(n)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое число')
+    print()
+print()
+
+total = 0
+while n >= 0:
+    total += n
+    while True:
+        n = input('Введи целое число: ')
+        if_number = ifnumber.if_number(n)
+        if if_number == 'int':
+            n = int(n)
+            break
+        else:
+            print('Данные введены некорректно! Нужно ввести целое число')
+        print()
+    print()
+
+print(f"{total}")
