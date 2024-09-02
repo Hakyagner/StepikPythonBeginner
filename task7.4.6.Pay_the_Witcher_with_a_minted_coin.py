2da1a05cdb5e84d2a333ea2ea1e03e80e60af96f
Hakyagner <katy.roj.learn@mail.ru>
2024-09-02 19:43:54 +0300

    fix 7.4.6

---
+++ task7.4.6.Pay_the_Witcher_with_a_minted_coin.py
@@ -0,0 +1,29 @@
+import ifnumber
+print("Ведьмаку заплатите чеканной монетой")
+print()
+
+while True:
+    summa = input('Введи целое число: ')
+    if_number = ifnumber.if_number(summa)
+    if if_number == 'int':
+        summa = int(summa)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое число')
+    print()
+print()
+
+total = 0
+
+while summa != 0:
+    if summa >= 25:
+        summa -= 25
+    elif 10 <= summa < 25:
+        summa -= 10
+    elif 5 <= summa < 10:
+        summa -= 5
+    else:
+        summa -= 1
+    total += 1
+
+print(f"Ведьмаку нужно отдать минимум {total} чеканных монет.")
