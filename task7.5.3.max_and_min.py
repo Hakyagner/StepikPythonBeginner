9183a7ab428572c0178cf1cb9ce2b5aee282fb8b
Hakyagner <katy.roj.learn@mail.ru>
2024-10-06 19:14:45 +0300

    fix 7.5.3

---
+++ task7.5.3.max_and_min.py
@@ -0,0 +1,27 @@
+import ifnumber
+print("max и min")
+print()
+
+maksim = -1
+minim = 10
+while True:
+    n = input('Введи целое число больше 9: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int' and int(n) >= 10:
+        n = int(n)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое число больше 9')
+    print()
+print()
+
+while n != 0:
+    last_digit = n % 10
+    if maksim < last_digit:
+        maksim = last_digit
+    if minim > last_digit:
+        minim = last_digit
+    n = n // 10
+
+print(f"Максимальная цифра равна {maksim}.")
+print(f"Минимальная цифра равна {minim}.")
