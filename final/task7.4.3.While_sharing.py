86d82e82d4a5564e9bc30285f8619249915c221c
Alex Ko <help.mail015@gmail.com>
2024-10-06 19:19:15 +0300

    find errors 7.4.3 - 7.5.2

--- final/task7.4.3.While_sharing.py
+++ final/task7.4.3.While_sharing.py
@@ -1,8 +1,7 @@
 import ifnumber
-print("Сумма чисел")
+print("Пока делимся")
 print()
 
-all_s = ""
 while True:
     n = input('Введи целое число: ')
     if_number = ifnumber.if_number(n)
@@ -14,15 +13,10 @@ while True:
     print()
 print()
 
-total = 0
-while n >= 0:
-    total += n
-    if all_s == "":
-        all_s += str(n)
-    else:
-        all_s += ", " + str(n)
+while n % 7 == 0:
+    print(n)
     while True:
-        n = input('Введи целое число: ')
+        n = input('Введи следующие целое число: ')
         if_number = ifnumber.if_number(n)
         if if_number == 'int':
             n = int(n)
@@ -30,8 +24,3 @@ while n >= 0:
         else:
             print('Данные введены некорректно! Нужно ввести целое число')
         print()
-    print()
-
-print(f'Сумма всех членов последовательности "{all_s}" равна {total}')
-
-# done
--- final/task7.4.4.Sum_of_numbers.py
+++ final/task7.4.4.Sum_of_numbers.py
@@ -1,7 +1,8 @@
 import ifnumber
-print("Пока делимся")
+print("Сумма чисел")
 print()
 
+all_s = ""
 while True:
     n = input('Введи целое число: ')
     if_number = ifnumber.if_number(n)
@@ -13,10 +14,15 @@ while True:
     print()
 print()
 
-while n % 7 == 0:
-    print(n)
+total = 0
+while n >= 0:
+    total += n
+    if all_s == "":
+        all_s += str(n)
+    else:
+        all_s += ", " + str(n)
     while True:
-        n = input('Введи следующие целое число: ')
+        n = input('Введи целое число: ')
         if_number = ifnumber.if_number(n)
         if if_number == 'int':
             n = int(n)
@@ -24,5 +30,6 @@ while n % 7 == 0:
         else:
             print('Данные введены некорректно! Нужно ввести целое число')
         print()
+    print()
 
-# done
+print(f'Сумма всех членов последовательности "{all_s}" равна {total}')
