f2306023c64871c8c3dd762569ebcc06431215e3
Hakyagner <katy.roj.learn@mail.ru>
2024-11-05 22:22:52 +0200

    fix 7.8.1

---
+++ task7.8.1.Table_1.py
@@ -0,0 +1,18 @@
+import ifnumber
+print("Таблица-1")
+print()
+
+while True:
+    n = input('Введи целое число, меньше 9: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int' and int(n) < 9:
+        n = int(n)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое число, меньше 9')
+        print()
+
+for i in range(n):
+    print(n, n, n)
+
+# Если вдруг надо писать ещё что-то в print, то подскажи что надо !
