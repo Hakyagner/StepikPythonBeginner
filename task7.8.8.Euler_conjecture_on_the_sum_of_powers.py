95c171e74c0cb1d045f5421b11529f4598275e21
Hakyagner <katy.roj.learn@mail.ru>
2024-11-12 00:27:41 +0200

    fix 7.8.8

--- task7.8.8.Euler_conjecture_on_the_sum_of_powers.py
+++ task7.8.8.Euler_conjecture_on_the_sum_of_powers.py
@@ -7,8 +7,9 @@ for a in range(1, limit + 1):
     for b in range(a, limit + 1):
         for c in range(b, limit + 1):
             for d in range(c, limit + 1):
-                e = int((a ** 5 + b ** 5 + c ** 5 + d ** 5) ** (1 / 5))    # a5 + b5 + c5 + d5 = e5.
-                if a ** 5 + b ** 5 + c ** 5 + d ** 5 == e ** 5:
-                    print(f"Найденные числа: a = {a}, b = {b}, c = {c}, d = {d}, e = {e}")
+                summa_stepeney = a ** 5 + b ** 5 + c ** 5 + d ** 5
+                e = int(summa_stepeney ** (1 / 5))    # a5 + b5 + c5 + d5 = e5.
+                if summa_stepeney == e ** 5:
+                    print(f"{a} ** 5 + {b} ** 5 + {c} ** 5 + {d} ** 5 = {e} ** 5 = {e ** 5}")
                     print(f"Сумма чисел a, b, c, d, e равна {a + b + c + d + e}")
                     break
