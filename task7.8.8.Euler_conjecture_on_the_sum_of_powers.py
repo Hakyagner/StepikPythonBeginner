4cb5971f0c8bde4d39c28c61bfd04183cd8ecd16
Alex Ko <help.mail015@gmail.com>
2024-11-15 02:14:59 +0200

    Find problems 7.8.3, 7.8.7, 7.8.8

--- task7.8.8.Euler_conjecture_on_the_sum_of_powers.py
+++ task7.8.8.Euler_conjecture_on_the_sum_of_powers.py
@@ -8,7 +8,7 @@ for a in range(1, limit + 1):
         for c in range(b, limit + 1):
             for d in range(c, limit + 1):
                 summa_stepeney = a ** 5 + b ** 5 + c ** 5 + d ** 5
-                e = int(summa_stepeney ** (1 / 5))    # a5 + b5 + c5 + d5 = e5.
+                e = int(summa_stepeney ** (1 / 5))
                 if summa_stepeney == e ** 5:
                     print(f"{a} ** 5 + {b} ** 5 + {c} ** 5 + {d} ** 5 = {e} ** 5 = {e ** 5}")
                     print(f"Сумма чисел a, b, c, d, e равна {a + b + c + d + e}")
