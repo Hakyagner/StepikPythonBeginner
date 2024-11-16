6ecb024d7cd9965e24fb92a095b16ab5a28bc141
Hakyagner <katy.roj.learn@mail.ru>
2024-11-17 01:12:09 +0200

    fix 7.9.1

--- task7.8.3.Table_3.py
+++ task7.8.3.Table_3.py
@@ -11,9 +11,11 @@ while True:
     else:
         print('Данные введены некорректно! Нужно ввести целое число, меньше или равно 9')
         print()
+print()
 
 for i in range(1, n + 1):
     for j in range(1, 10):
         print(i, '+', j, '=', i + j)
+    print()
 
-# Неправильно. Как должен выглядеть ответ?
+# done
